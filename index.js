c125fc943ace1bc1111eec1dd237fdc793b919bb
Orji Cecilia <ceciliaorji.co@gmail.com>
2020-03-27 21:47:43 +0100

    JavaScript Task 1

---
+++ index.js
@@ -0,0 +1,22 @@
+ let name = "Orji Cecilia";
+
+console.log("My name is " + name);
+
+var courses = ["Front-end", "Coding"];
+console.log("The courses I'll be offering in Startng are; " + courses);
+
+const data = [];
+
+for (let i=0; i<=100; i++) {
+
+  if (i%2===0) {
+
+    data.push(i);
+
+  }
+
+}
+
+console.log(data);
+
+
